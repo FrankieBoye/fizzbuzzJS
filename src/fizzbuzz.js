6db5f3fb7c7445201fb63cb485a688e63891787c
Carly <carly.jenkinson65@googlemail.com>
2019-10-07 12:33:59 +0100

    Return true if divisible by 3.

---
+++ src/fizzbuzz.js
@@ -0,0 +1,9 @@
+module.exports=class Fizzbuzz {
+  constructor () {  
+  }
+
+  isDivisbleByThree (number) {
+    return true
+  }
+}
+
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,8 +1,13 @@
-var Fizzbuzz = require('../src/fizzbuzzJS')
+var Fizzbuzz = require('../src/fizzbuzz')
 
 describe('Fizzbuzz', function(){
   it('knows when a number is divisble by three', function() {
     var fizzbuzz = new Fizzbuzz()
     expect(fizzbuzz.isDivisbleByThree(3)).toBe(true);
+  })
+
+  it('knows when a number is not divisble by three', function() {
+    var fizzbuzz = new Fizzbuzz()
+    expect(fizzbuzz.isDivisbleByThree(5)).toBe(false);
   })
 })
