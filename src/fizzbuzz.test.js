f7ab0f39b361d00fab2563d919a607fc6738eba7
FrankieBoye <frankie4@btinternet.com>
2019-10-08 09:37:54 +0100

    refactored tests to remove repetition of fizzbuzz = new Fizzbuzz

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,33 +1,33 @@
 var Fizzbuzz = require('../src/fizzbuzz')
 
+var fizzbuzz = function(){}
+
+beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
+
 describe('Fizzbuzz', function(){
   it('knows when a number is divisble by three', function() {
-    var fizzbuzz = new Fizzbuzz()
     expect(fizzbuzz.isDivisbleByThree(3)).toBe(true);
   })
 
   it('knows when a number is not divisble by three', function() {
-    var fizzbuzz = new Fizzbuzz()
     expect(fizzbuzz.isDivisbleByThree(5)).toBe(false);
   })
 
   it('knows when a number is divisble by five', function() {
-    var fizzbuzz = new Fizzbuzz()
     expect(fizzbuzz.isDivisbleByFive(5)).toBe(true);
   })
 
   it('knows when a number is not divisble by five', function() {
-    var fizzbuzz = new Fizzbuzz()
     expect(fizzbuzz.isDivisbleByFive(7)).toBe(false);
   })
 
   it('knows when a number is divisble by both three and five', function() {
-    var fizzbuzz = new Fizzbuzz()
     expect(fizzbuzz.isDivisbleByFifteen(15)).toBe(true);
   })
 
   it('knows when a number is not divisble by both three and five', function() {
-    var fizzbuzz = new Fizzbuzz()
     expect(fizzbuzz.isDivisbleByFifteen(16)).toBe(false);
   })
 
